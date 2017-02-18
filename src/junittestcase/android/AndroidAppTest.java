b500c9550fba08f1b5cb268bc38bd1686ae27af8
baozhida <baozhida88@126.com>
2017-02-19 02:18:36 +0800

    Update and rename AndroidApp.java to AndroidAppTest.java

--- src/junittestcase/android/AndroidAppTest.java
+++ src/junittestcase/android/AndroidAppTest.java
@@ -17,7 +17,7 @@ import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class AndroidIndexPageCategoryTest {
+public class AndroidAppTest {
     static MacacaClient driver;
     boolean isFail;
     
